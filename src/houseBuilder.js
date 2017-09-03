09a5179663a5e57019af17dca0622ddf01f001ef
indra-uolles <smirnovanatalia2008@gmail.com>
2017-09-03 22:55:58 +0300

    happy boy animation

--- src/houseBuilder.js
+++ src/houseBuilder.js
@@ -5,53 +5,7 @@ function HouseBuilder(g, babkaFrames) {
     this.windows = g.group();
     this.roofs = g.group();
     this.babkas = g.group();
-}
-
-HouseBuilder.prototype._addHousePart = function(name, x, y, i) {
-    var houseSprite = this.g.sprite(name);
-
-    houseSprite.x = x;
-    houseSprite.y = y;
-
-    if (name.indexOf("chimney") != - 1 || name.indexOf("window") != -1 || name.indexOf("roof") != -1) {
-        houseSprite.index = i;
-    }
-
-    if (name.indexOf("chimney") != - 1) {
-        houseSprite.hit = false;
-        this.chimneys.addChild(houseSprite);
-    } else if (name.indexOf("window") != -1) {
-        this.windows.addChild(houseSprite);
-        var babkaFrames = [
-          "window.png",
-          "window.png",
-          "frame_00.gif",
-          "frame_01.gif",
-          "frame_02.gif",
-          "frame_03.gif",
-          "frame_04.gif",
-          "frame_05.gif",
-          "frame_06.gif",
-          "frame_07.gif",
-          "frame_08.gif",
-          "frame_09.gif",
-          "window.png"
-        ];
-
-        var babka = g.sprite(babkaFrames);
-        babka.x = houseSprite.x;
-        babka.y = houseSprite.y;
-        babka.loop = false;
-        babka.index = houseSprite.index;
-        babka.visible = false;
-        this.babkas.addChild(babka);
-
-    } else if (name.indexOf("roof") != -1) {
-        houseSprite.hit = false;
-        this.roofs.addChild(houseSprite);
-    } else {
-        this.houses.addChild(houseSprite);
-    }
+    this.boys = g.group();
 }
 
 HouseBuilder.prototype.getTwoBoysFirstBad = function(x, y, i) {
@@ -172,4 +126,100 @@ HouseBuilder.prototype.showBabka = function(babka) {
     babka.visible = true;
     babka.loop = false;
     babka.play();
+}
+
+HouseBuilder.prototype.showBoy = function(boy) {
+    boy.visible = true;
+    boy.loop = false;
+    boy.play();
+}
+
+HouseBuilder.prototype._addHousePart = function(name, x, y, i) {
+    var houseSprite = this.g.sprite(name);
+
+    houseSprite.x = x;
+    houseSprite.y = y;
+
+    if (name.indexOf("chimney") != - 1 || name.indexOf("window") != -1 || name.indexOf("roof") != -1) {
+        houseSprite.index = i;
+    }
+
+    if (name.indexOf("chimney") != - 1) {
+        houseSprite.hit = false;
+        this.chimneys.addChild(houseSprite);
+
+    } else if (name.indexOf("window") != -1) {
+        this.windows.addChild(houseSprite);
+        this._createBabkaAnim(houseSprite);
+        this._createHappyBoyAnim(houseSprite);
+
+    } else if (name.indexOf("roof") != -1) {
+        houseSprite.hit = false;
+        this.roofs.addChild(houseSprite);
+
+    } else {
+        this.houses.addChild(houseSprite);
+    }
+}
+
+HouseBuilder.prototype._createBabkaAnim = function(window) {
+    var babkaFrames = [
+        "window.png",
+        "window.png",
+        "babka_frame_00.gif",
+        "babka_frame_01.gif",
+        "babka_frame_02.gif",
+        "babka_frame_03.gif",
+        "babka_frame_04.gif",
+        "babka_frame_05.gif",
+        "babka_frame_06.gif",
+        "babka_frame_07.gif",
+        "babka_frame_08.gif",
+        "babka_frame_09.gif",
+        "window.png"
+    ];
+    var babka = g.sprite(babkaFrames);
+
+    babka.x = window.x;
+    babka.y = window.y;
+    babka.loop = false;
+    babka.index = window.index;
+    babka.visible = false;
+    this.babkas.addChild(babka);
+}
+
+HouseBuilder.prototype._createHappyBoyAnim = function(window) {
+    var boyFrames = [
+        "window.png",
+        "window.png",
+        "happyboy_frame_00.gif",
+        "happyboy_frame_01.gif",
+        "happyboy_frame_02.gif",
+        "happyboy_frame_03.gif",
+        "happyboy_frame_04.gif",
+        "happyboy_frame_05.gif",
+        "happyboy_frame_06.gif",
+        "happyboy_frame_07.gif",
+        "happyboy_frame_08.gif",
+        "happyboy_frame_09.gif",
+        "happyboy_frame_10.gif",
+        "happyboy_frame_11.gif",
+        "happyboy_frame_12.gif",
+        "happyboy_frame_13.gif",
+        "happyboy_frame_14.gif",
+        "happyboy_frame_15.gif",
+        "happyboy_frame_16.gif",
+        "happyboy_frame_17.gif",
+        "happyboy_frame_18.gif",
+        "happyboy_frame_19.gif",
+        "window.png"
+    ];
+    var boy = g.sprite(boyFrames);
+
+    boy.x = window.x;
+    boy.y = window.y;
+    boy.loop = false;
+    boy.index = window.index;
+    boy.visible = false;
+    this.boys.addChild(boy);
 }
